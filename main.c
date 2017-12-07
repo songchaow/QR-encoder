32179a40de1c848cbf71a936b029a07a529c83e4
songchaow <songchaow@outlook.com>
2017-12-07 16:18:49 +0800

    add implementation of `itoa`

--- main.c
+++ main.c
@@ -6,6 +6,62 @@
 #include <math.h>
 #include <string.h>
 
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        char temp;
+        temp = *(str+start);
+        *(str+start) = *(str+end);
+        *(str+end) = temp;
+        start++;
+        end--;
+    }
+}
+
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+ 
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}
+
 unsigned int calalign_n(int ver) //计算校正图形数，同时为校正图形坐标赋值
 {
     if(ver==1) return 0;
